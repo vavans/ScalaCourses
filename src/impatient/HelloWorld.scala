79b5011fa6150864d0fbf3dd4ccfe6a75c234ac4
vavans <vannnns@gmail.com>
2013-09-28 21:33:34 +0200

    first commit

---
+++ src/impatient/HelloWorld.scala
@@ -0,0 +1,14 @@
+package impatient
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vans
+ * Date: 28/09/13
+ * Time: 20:42
+ * To change this template use File | Settings | File Templates.
+ */
+object HelloWorld {
+  def main(arg : Array[String]) {
+    println("kikoo");
+  }
+}
